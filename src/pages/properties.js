5fa4101b1068dadf5e1ec96b502d4a4ef840a9c2
M Faris Gibran <mfarisgibran@gmail.com>
2021-01-28 17:09:07 +0700

    Add basic components to properties page

--- src/pages/properties.js
+++ src/pages/properties.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Box, Flex, Container, Heading } from '@chakra-ui/react'
 
 export default function PropertiesPage() {
   return (
-    <div>
-      <h1>All properties</h1>
-    </div>
+    <Container>
+      <Heading as="h1">All properties</Heading>
+      <Flex id="properties-content">
+        <Box id="properties-cards">
+          <Box></Box>
+        </Box>
+        <Box id="properties-map"></Box>
+      </Flex>
+    </Container>
   )
 }
