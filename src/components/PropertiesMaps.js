33b6df813d492731f27992eb612593a97c0035b8
M Faris Gibran <mfarisgibran@gmail.com>
2021-01-28 21:09:41 +0700

    Add google maps to properties page

--- src/components/PropertiesMaps.js
+++ src/components/PropertiesMaps.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import { Box } from '@chakra-ui/react'
+import GoogleMapReact from 'google-map-react'
 
 export default function PropertiesMaps() {
   return (
     <Box id="properties-map" width="50%" bg="white">
-      Maps
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
+        defaultCenter={{ lat: 59.95, lng: 30.33 }}
+        defaultZoom={11}
+      >
+        <p>Example</p>
+      </GoogleMapReact>
     </Box>
   )
 }
